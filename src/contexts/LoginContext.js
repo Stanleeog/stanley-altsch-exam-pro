a8302d1256122a3db6cf33b352051e9a9ddc3930
Stanley Oguh <ebuksworks@gmail.com>
2022-11-12 11:09:47 +0100

    Css and Log in

--- src/contexts/LoginContext.js
+++ src/contexts/LoginContext.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import { createContext } from "react";
 
 export const LoginContext = createContext({})
-=======
-import { createContext } from "react";
 
-export const LoginContext = createContext({})
->>>>>>> f10a2e70463cb17bd7ad973082dbb0a490f03127
+;
